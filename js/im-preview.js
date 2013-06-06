4c48b0a18b514f47e6d6257efc5ceb4db0c96a17
Lewis Gallacher <lewis.gallacher@opensquare.co.uk>
2013-06-06 15:00:26 +0100

    refactored preview js, made destination and search terms visibility configurable in merge-flow.properties

--- js/im-preview.js
+++ js/im-preview.js
@@ -1,128 +1,56 @@
 function IMPreview($container, im){
 	var imInstance = im;
-	var payload;
-	var payloadHeader = '';
 	var $mainContainer = $container;
-	var $previewPane;
-	var _this = this;
-	var tabs = {
-		'payload' : {
-			'text': 'Data',
-			'group' : 'payload'
-		},
-		'preview' : {
-			'text' : 'Merge Preview',
-			'group' : 'merge'
-		}
+
+	var previewTabs = {
+		'payload' : 'payload',
+		'preview' : 'preview'
 	}
+
+	var $previewPane;
+	var tabManager;
+	var pollSettings;
+
+	var _this = this;
 	
 	this.init = function(){
-		var $paneTemplate = $('<div class="previewPane closed"><div class="tabs"></div><div class="contents"></div></div>');
+		var $paneTemplate = $('<div class="previewPane closed"></div>');
 		$mainContainer.append($paneTemplate);
 		$previewPane = $('.previewPane', $mainContainer);
+		tabManager = new TabManager($previewPane);
+		//pollSettings = pollSettings
 	}
 	this.onReady = function(flowId, jobId, previewRequested){
-		reloadTabs(previewRequested);
+		tabManager.emptyTabs();
+		tabManager.addTab(previewTabs.payload);
 		this.loadPayloadData(flowId);
 		if (previewRequested === 'true') {
-			addTab(tabs.preview);
-			this.showMergePreview();
+			tabManager.addTab(previewTabs.preview);
+			this.loadMergePreview();
 		}
-	}
-	function reloadTabs(){
-		$previewPane.find('.tabs').empty();
-		$previewPane.find('.contents').empty();
-		addTab(tabs.payload);
-	}
-	
-	function addTab(tab){
-		var $tabTemplate = $('<div class="tabContainer"><h4 class="previewTab"><span class="tabText"></span><span class="toggle toggleOpen"/></h4></div>');
-		$tabTemplate.data('group', tab.group);
-		$tabTemplate.find('.tabText').text(tab.text);
-		$tabTemplate.click(function(){
-			_this.toggle(this);
-		});
-		var $content = $('<div class="previewContent"></div>').data('group', tab.group);
-		$previewPane.find('.tabs').append($tabTemplate);
-		$previewPane.find('.contents').append($content);
 	}
 
 	this.loadPayloadData = function(flowId){
+		var tmanager = tabManager;
 		$.get('rhinoforms/data-document/' + flowId, function(data){
-			_this.insertPayloadData(data.childNodes[0].getElementsByTagName('mmJob')[0]);
+			tmanager.setContent(previewTabs.payload, _this.formatPayloadData(data.childNodes[0].getElementsByTagName('mmJob')[0]));
 		});
 	}
-	this.insertPayloadData = function(payloadXML){
-		payload = parsePayload(payloadXML.getElementsByTagName('data')[0]);
-		$payloadContent = $('<div class="payloadData"></div>').append(payloadHeader).append(payload);
-		var $tabContent = findContent(tabs.payload.group, '.contents .previewContent');
-		$tabContent.find('.payloadData').remove();
-		$tabContent.append($payloadContent);
+
+	this.formatPayloadData = function(payloadXML){
+		var payload = parsePayload(payloadXML.getElementsByTagName('data')[0]);
+		$payloadContent = $('<div class="payloadData"></div>').append(payload);
+		return $payloadContent;
 	}
 
-	this.showMergePreview = function(){
-		findContent(tabs.preview.group, '.tabContainer').click();
+	this.loadMergePreview = function(){
+		tabManager.toggle(previewTabs.preview);
 	}
 
 	this.close = function(){
-		$previewPane.removeClass('open');
-		$('.previewTab .toggle', $previewPane).removeClass('toggleClose');
-		$('.tabContainer.active, ', $previewPane).removeClass('active');
-		$('.contents .previewContent.active, ', $previewPane).removeClass('active');
-		$mainContainer.removeClass('show-preview');
-		return this;
+		tabManager.closePanel();
 	}
 
-	this.open = function(){
-		$previewPane.addClass('open');
-		$('.previewTab .toggle', $previewPane).addClass('toggleClose');
-		$mainContainer.addClass('show-preview');
-		return this;
-	}
-
-	this.toggle = function(tabContainer){
-		for (var tab in tabs) {
-			$previewPane.removeClass(tabs[tab].group);
-		}
-		if($previewPane.hasClass('open')){
-			if (tabContainer && !$(tabContainer).hasClass('active')){
-				switchTab(tabContainer, tabs);
-			} else {
-				this.close();
-			}
-		} else {
-			if(tabContainer){
-				switchTab(tabContainer,tabs);
-
-			}
-			this.open();
-		}
-		return this;
-	}
-
-	function switchTab(tabContainer, tabs){
-		out(tabs);
-		var tabGroup = $(tabContainer).data('group');
-		$('.tabContainer.active, ', $previewPane).removeClass('active');
-		$('.contents .previewContent.active, ', $previewPane).removeClass('active');
-		findContent(tabGroup,'.contents .previewContent').addClass('active');
-		findContent(tabGroup,'.tabContainer').addClass('active');
-		$previewPane.addClass(tabGroup);
-
-	}
-
-	function findContent(group, selector){
-		var $contents = $previewPane.find(selector);
-		var group = group;
-		var content = $();
-		$contents.each(function(){
-			if ($(this).data('group') === group){
-				content = $(this);
-				return;
-			}
-		});
-		return content;
-	}
 
 	function parsePayload(payloadXML, sub){
 		var list = $('<ul>');
@@ -136,7 +64,7 @@ function IMPreview($container, im){
 				} else {
 					var listItem = $('<li>');
 					var subName = node.getAttribute('name') != null ? ' - ' + node.getAttribute('name') : '';
-					listItem.append('<span><strong>' + node.nodeName + '</strong>:' + subName + '</span>').append(parsePayload(node, true));	
+					listItem.append('<span><strong>' + node.nodeName + '</strong>:' + subName + '</span>').append(parsePayload(node, true));
 					list.append(listItem);
 				}
 				
@@ -173,5 +101,114 @@ function IMPreview($container, im){
 	}
 
 	this.init();
+
+	function TabManager(container){
+		var _this = this;
+		var $container = container;
+		var tabs = {
+			'payload' : {
+				'text': 'Data',
+				'group' : 'payload',
+				'name' : 'payload'
+			},
+			'preview' : {
+				'text' : 'Merge Preview',
+				'group' : 'merge',
+				'name' : 'preview'
+			}
+		}
+		var tabContainerSelector = '.tabs .tabContainer';
+		var tabContentSelector = '.contents .previewContent';
+
+
+		this.init = function(){
+			$container.empty().append('<div class="tabs"></div><div class="contents"></div>')
+		}
+
+		this.emptyTabs = function(){
+			$container.find('.tabs').empty();
+			$container.find('.contents').empty();
+		}
+
+		this.addTab = function(tab){
+			var tab = tabs[tab];
+			var $tabTemplate = $('<div class="tabContainer"><h4 class="previewTab"><span class="tabText"></span><span class="toggle toggleOpen"/></h4></div>').data('group', tab.group);
+			$tabTemplate.find('.tabText').text(tab.text);
+
+			var $content = $('<div class="previewContent"></div>').data('group', tab.group);
+
+			$tabTemplate.click(function(){
+				_this.toggle(tab.name);
+			});
+			
+			$container.find('.tabs').append($tabTemplate);
+			$container.find('.contents').append($content);
+		}
+
+		this.setContent = function(tab, content){
+			findTabPart(tabs[tab].group, tabContentSelector).empty().append(content);
+		}
+
+		this.switchTo = function(tab){
+			var activeTab = tabs[tab];
+			for (var t in tabs) {
+				if (activeTab && t == activeTab.name){
+					$container.addClass(activeTab.group);
+					findTabPart(activeTab.group, tabContainerSelector).addClass('active');
+					findTabPart(activeTab.group, tabContentSelector).addClass('active');
+					activeTab.active = true;
+				} else {
+					$container.removeClass(tabs[t].group);
+					findTabPart(tabs[t].group, tabContainerSelector).removeClass('active');
+					findTabPart(tabs[t].group, tabContentSelector).removeClass('active');
+					tabs[t].active = false;
+				} 
+				
+			}
+		}
+
+		this.toggle = function(tab){
+			if($container.hasClass('open')){
+				if (!tabs[tab].active){
+					this.switchTo(tab);
+				} else {
+					this.switchTo();
+					this.closePanel();
+				}
+			} else {
+				$container.addClass('open');
+				if (tab){
+					this.switchTo(tab);
+				}
+				this.openPanel();
+			}
+		}
+
+		this.openPanel = function(){
+			$container.addClass('open');
+			$('.previewTab .toggle', $container).addClass('toggleClose');
+			$mainContainer.addClass('show-preview');
+		}
+
+		this.closePanel = function(){
+			$container.removeClass('open');
+			$('.previewTab .toggle', $container).removeClass('toggleClose');
+			$mainContainer.removeClass('show-preview');
+		}
+
+		function findTabPart(group, selector){
+			var $contents = $container.find(selector);
+			var group = group;
+			var content = $();
+			$contents.each(function(){
+				if ($(this).data('group') === group){
+					content = $(this);
+					return;
+				}
+			});
+			return content;
+		}
+		this.init();
+	}
 
 }
